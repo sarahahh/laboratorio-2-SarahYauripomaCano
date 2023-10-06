e95024445f8c76c4f2258dc45d82cc77e2086c58
sarahahh <146607057+sarahahh@users.noreply.github.com>
2023-10-05 22:44:03 -0500

    Add files via upload

---
+++ lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Funciones de líneas rectas";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(743, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bienvenido usuario, vamos a resolver un sistema de ecuaciones 2x2 mediante el ing" +
+    "reso de dos funciones ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 273);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(525, 270);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ecuación 1 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(452, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ecuación 2 :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "y =mx + b",
+            "y – y1 =m (x – x1)"});
+            this.comboBox1.Location = new System.Drawing.Point(464, 175);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(129, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(148, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(302, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Elija el tipo de ecuación que desea calcular :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(245, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(325, 21);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "de la forma y = mx + b   ó   y - y1 = m (x - x1)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ lab2/lab2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //ESTO ES UN ERROR QUE NO PUEDO ELIMINAR
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //ESTO ES UN ERROR QUE NO PUEDO ELIMINAR
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //ESTO ES UN ERROR QUE NO PUEDO ELIMINAR
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // INICIO DE FUNCIONES 
+
+        // asignacion de tipo de funcion a la variable "seleccion"
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String seleccion = comboBox1.SelectedItem.ToString();
+        }
+
+        // encontrar datos por medio de string
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // variables
+            String ec1;
+            int indexM1;
+            int indexX1;
+            String pendiente1;
+            int indexB1;
+            String intercep1;
+            //
+            String ec2;
+            int indexM2;
+            int indexX2;
+            String pendiente2;
+            int indexB2;
+            String intercep2;
+            //
+            int indexB12;
+            int indexB22;
+
+            // seleccion = y = mx + b
+
+            if (comboBox1.SelectedItem.ToString() == "y =mx + b")
+            {
+                // rango de ec1 (y =mx + b)
+
+                ec1 = textBox1.Text;
+
+                indexM1 = ec1.IndexOf('=') + 1;
+                indexX1 = ec1.IndexOf('x') - 2;
+
+                pendiente1 = ec1.Substring(indexM1, indexX1);
+
+                indexB1 = ec1.IndexOf('+') + 1;
+
+                intercep1 = ec1.Substring(indexB1);
+
+                // rango de ec2 (y = mx + b)
+
+                ec2 = textBox2.Text;
+
+                indexM2 = ec2.IndexOf('=') + 1;
+                indexX2 = ec2.IndexOf('x') - 2;
+
+                pendiente2 = ec2.Substring(indexM2, indexX2);
+
+                indexB2 = ec2.IndexOf('+') + 1;
+
+                intercep2 = ec2.Substring(indexB2);
+
+                // llamar funcion
+
+                float pendiente1Float = float.Parse(pendiente1.Trim()[0].ToString());
+                float pendiente2Float = float.Parse(pendiente2.Trim()[0].ToString());
+                var intercep1Float = float.Parse(intercep1.Trim());
+                float intercep2Float = float.Parse(intercep2.Trim());
+
+                operaciones1(pendiente1Float, pendiente2Float, intercep1Float, intercep2Float);
+
+            }
+
+            // seleccion = y - y1 = m (x - x1 )
+
+            else if (comboBox1.SelectedItem.ToString() == "y – y1 =m (x – x1)")
+            {
+                // rango de ec1 ( y - y1 = m (x - x1 ) )
+
+                ec1 = textBox1.Text;
+
+                indexB1 = ec1.IndexOf('-') + 1;
+                indexB12 = ec1.IndexOf('=') - 2;
+
+                intercep1 = ec1.Substring(indexB1, indexB12);
+
+                indexM1 = ec1.IndexOf('=') + 1;
+                indexX1 = ec1.IndexOf('(') - 2;
+
+                pendiente1 = ec1.Substring(indexM1, indexX1);
+
+                //rango de ec2 ( y - y1 = m (x - x1 ) )
+
+                ec2 = textBox2.Text;
+
+                indexB2 = ec1.IndexOf('-') + 1;
+                indexB22 = ec1.IndexOf('=') - 2;
+
+                intercep2 = ec2.Substring(indexB2, indexB22);
+
+                indexM2 = ec1.IndexOf('=') + 1;
+                indexX2 = ec1.IndexOf('(') - 2;
+
+                pendiente2 = ec2.Substring(indexM2, indexX2);
+
+                // llamar funcion 
+                float pendiente1Float = float.Parse(pendiente1.Trim()[0].ToString());
+                float pendiente2Float = float.Parse(pendiente2.Trim()[0].ToString());
+                float intercep1Float = float.Parse(intercep1.Trim());
+                float intercep2Float = float.Parse(intercep2.Trim());
+
+                operaciones2(pendiente1Float, pendiente2Float, intercep1Float, intercep2Float);
+
+            }
+        }
+
+        // funcion para hallar solucion de la forma (y = mx + b)
+
+        public void operaciones1(float pendiente1, float intercep1, float pendiente2, float intercep2)
+        {
+            if (pendiente1 == pendiente2)
+            {
+                MessageBox.Show("Las rectas son paralelas");
+            }
+            else if (pendiente1 != pendiente2)
+            {
+                float result1 = pendiente1 * pendiente2;
+                float result2 = (intercep2 - intercep1) / (pendiente1 - pendiente2);
+                float result3 = (pendiente1 * result2) + intercep1;
+
+                if (result1 == -1)
+                {
+                    MessageBox.Show("Las rectas son perpendiculares");
+
+                }
+                else
+                {
+                    MessageBox.Show("Las rectas se cruzan en :" + "(" + result2 + "," + result3 + ")");
+
+                }
+            }
+
+        }
+
+        // funcion para hallar solucion de la forma ( y - y1 = m (x - x1 ) )
+
+        public void operaciones2(float pendiente1, float intercep1, float pendiente2, float intercep2)
+        {
+            float result1 = pendiente1 * pendiente2;
+            float result2 = (intercep2 - intercep1) / (pendiente1 - pendiente2);
+            float result3 = (pendiente1 * result2) + intercep1;
+
+            if (pendiente1 == pendiente2)
+            {
+                MessageBox.Show("Las rectas son paralelas");
+            }
+            else if (pendiente1 != pendiente2)
+            {
+                if (result1 == -1)
+                {
+                    MessageBox.Show("Las rectas son perpendiculares");
+                }
+                else
+                {
+                    MessageBox.Show("Las rectas se cruzan en :" + "(" + result2 + "," + result3 + ")");
+                }
+            }
+        }
+    }
+}
+
